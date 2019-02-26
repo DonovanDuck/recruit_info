a8b6ddc0a65c17b36ba8a1b8c2900f48144278d3
ajaxGX <m15735042025@163.com>
2019-02-26 12:03:47 +0800

    d

--- src/test/java/cn/edu/tit/UserTest.java
+++ src/test/java/cn/edu/tit/UserTest.java
@@ -9,12 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-<<<<<<< HEAD:src/test/java/cn/edu/tit/AdminTest.java
 import com.alibaba.fastjson.JSONObject;
 
-=======
 import cn.edu.tit.bean.RecruitInfo;
->>>>>>> dd9bcbbe450a1cdca06914e3277e54d95eb0e7e4:src/test/java/cn/edu/tit/UserTest.java
 import cn.edu.tit.bean.User;
 import cn.edu.tit.idao.IUserDao;
 import cn.edu.tit.iservice.IUserService;
@@ -29,22 +26,12 @@ public class UserTest {
 	private IUserService iUserService;
 
 	@Resource
-<<<<<<< HEAD:src/test/java/cn/edu/tit/AdminTest.java
 	private IUserDao userDao;
 
 	@Test
 	public void Test() {
 		System.out.println(userDao.getApplyById("1"));
-=======
-	private IUserDao iUserDao;
 
-	@Test
-	public void Test() {
-		//		List<RecruitInfo> list = new ArrayList<RecruitInfo>();
-		//		list = iUserService.getRecruitInfo(null);
-		//		for (RecruitInfo recruitInfo : list) {
-		//			System.out.println(recruitInfo.toString());
->>>>>>> dd9bcbbe450a1cdca06914e3277e54d95eb0e7e4:src/test/java/cn/edu/tit/UserTest.java
 	}
 }
 
